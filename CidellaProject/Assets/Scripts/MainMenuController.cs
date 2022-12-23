623519f0867a72af534f10b86ba0e61d8b7dcec2
Ibrahim Aslan <i.aslan2020@gtu.edu.tr>
2022-12-24 00:52:35 +0300

    Ana Menü Paneller hariç bitti

--- CidellaProject/Assets/Scripts/MainMenuController.cs
+++ CidellaProject/Assets/Scripts/MainMenuController.cs
@@ -1,17 +1,38 @@
 using UnityEngine;
 using DG.Tweening;
 using System.Collections;
-using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using UnityEditor;
 
 public class MainMenuController : MonoBehaviour
 {
+    #region Header BUTTONS
+    [Space(10)]
+    [Header("BUTTONS")]
+    #endregion
+    [SerializeField] private RectTransform playButton;
+    [SerializeField] private RectTransform gameplayButton;
+    [SerializeField] private RectTransform settingsButton;
+    [SerializeField] private RectTransform creditsButton;
+    [SerializeField] private RectTransform quitButton;
+
+    #region Header PANELS
+    [Space(10)]
+    [Header("PANELS")]
+    #endregion
+    [SerializeField] private RectTransform gameplayPanel;
+    [SerializeField] private RectTransform settingsPanel;
+    [SerializeField] private RectTransform creditsPanel;
+
+    [Space(10)]
     [SerializeField] private CanvasGroup fadeScreen;
     [SerializeField] private CanvasGroup gameName;
-    [SerializeField] private Button playButton;
-    [SerializeField] private Button gameplayButton;
-    [SerializeField] private Button settingsButton;
-    [SerializeField] private Button quitButton;
-    private float buttonOpeningTimer = .5f;
+
+    #region Timer Variables
+    private readonly float buttonOpeningTimer = .6f;
+    private readonly float fadeTimer = 2f;
+    private readonly float panelTimer = .75f;
+    #endregion
 
     private void Start()
     {
@@ -20,16 +41,113 @@ public class MainMenuController : MonoBehaviour
 
     private IEnumerator StartUI()
     {
-        fadeScreen.DOFade(0f, 2f);
-        yield return new WaitForSeconds(2f);
-        gameName.DOFade(1f, 2f);
-        yield return new WaitForSeconds(2f);
-        playButton.GetComponent<RectTransform>().DOScale(1f, buttonOpeningTimer);
+        fadeScreen.DOFade(0f, fadeTimer);
+        yield return new WaitForSeconds(fadeTimer);
+
+        gameName.DOFade(1f, fadeTimer);
+        yield return new WaitForSeconds(fadeTimer);
+        
+        playButton.DOScale(1f, buttonOpeningTimer).SetEase(Ease.InQuint);
         yield return new WaitForSeconds(buttonOpeningTimer);
-        gameplayButton.GetComponent<RectTransform>().DOScale(1f, buttonOpeningTimer);
+        
+        gameplayButton.DOScale(1f, buttonOpeningTimer).SetEase(Ease.InQuint);
         yield return new WaitForSeconds(buttonOpeningTimer);
-        settingsButton.GetComponent<RectTransform>().DOScale(1f, buttonOpeningTimer);
+        
+        settingsButton.DOScale(1f, buttonOpeningTimer).SetEase(Ease.InQuint);
         yield return new WaitForSeconds(buttonOpeningTimer);
-        quitButton.GetComponent<RectTransform>().DOScale(1f, buttonOpeningTimer);
+        
+        creditsButton.DOScale(1f, buttonOpeningTimer).SetEase(Ease.InQuint);
+        yield return new WaitForSeconds(buttonOpeningTimer);
+        
+        quitButton.DOScale(1f, buttonOpeningTimer).SetEase(Ease.InQuint);
+        yield return new WaitForSeconds(buttonOpeningTimer);
+        
+        fadeScreen.blocksRaycasts = false;
     }
+
+    #region Buttons' OnClick Methods
+    #region Play Button
+    public void Play()
+    {
+        StartCoroutine(PlayRoutine());
+    }
+
+    private IEnumerator PlayRoutine()
+    {
+        fadeScreen.blocksRaycasts = true;
+        fadeScreen.DOFade(1f, fadeTimer);
+        yield return new WaitForSeconds(fadeTimer);
+        
+        SceneManager.LoadScene("MainGameScene");
+    }
+    #endregion
+
+    #region Gameplay Button
+    public void Gameplay()
+    {
+        StartCoroutine(OpenPanelRoutine(gameplayPanel));
+    }
+    #endregion
+
+    #region Settings Button
+    public void Settings()
+    {
+        StartCoroutine(OpenPanelRoutine(settingsPanel));
+    }
+    #endregion
+
+    #region Credits Button
+    public void Credits()
+    {
+        StartCoroutine(OpenPanelRoutine(creditsPanel));
+    }
+    #endregion
+
+    #region Quit Button
+    public void Quit()
+    {
+        StartCoroutine(QuitRoutine());
+    }
+
+    private IEnumerator QuitRoutine()
+    {
+        fadeScreen.blocksRaycasts = true;
+        fadeScreen.DOFade(1f, fadeTimer);
+        yield return new WaitForSeconds(fadeTimer);
+
+        if (EditorApplication.isPlaying)
+            EditorApplication.isPlaying = false;
+        else
+            Application.Quit();
+    }
+    #endregion
+
+    #region Back Button
+    public void Back(RectTransform panel)
+    {
+        StartCoroutine(BackRoutine(panel));
+    }
+
+    private IEnumerator BackRoutine(RectTransform panel)
+    {
+        panel.DOScale(0f, panelTimer);
+        fadeScreen.DOFade(0f, panelTimer);
+        yield return new WaitForSeconds(panelTimer);
+        panel.gameObject.SetActive(false);
+        fadeScreen.blocksRaycasts = false;
+    }
+    #endregion
+
+    #region Panel
+    private IEnumerator OpenPanelRoutine(RectTransform panel)
+    {
+        panel.gameObject.SetActive(true);
+        yield return null;
+        
+        fadeScreen.blocksRaycasts = true;
+        fadeScreen.DOFade(.5f, panelTimer);
+        panel.DOScale(1f, panelTimer).SetEase(Ease.OutQuint);
+    }
+    #endregion
+    #endregion
 }
